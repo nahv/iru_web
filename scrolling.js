53d82c3f50509a10a3bf9e5b872b8729ec67c8f4
nahvallejos <34092236+nahv@users.noreply.github.com>
2023-07-13 13:00:11 -0300

    Add files via upload

--- scrolling.js
+++ scrolling.js
@@ -11,7 +11,7 @@ var aboutScene = new ScrollMagic.Scene({
   .on('progress', function (e) {
     var progress = e.progress.toFixed(2);
     var scrollPos = progress * 400;
-    var gradientColors = 'linear-gradient(180deg, rgba(0, 0, 0, 1), rgba(232, 70, 59, 1), rgba(237, 187, 28, 1), rgba(255, 255, 255, 1), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) ' + scrollPos + '%)';
+    var gradientColors = 'linear-gradient(180deg, rgba(0, 0, 0, 1), rgba(232, 70, 59, 1), rgba(237, 187, 28, 1), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) ' + scrollPos + '%)';
     document.querySelector('.section#befabout').style.background = gradientColors;
 
     // Parallax Effect 
@@ -26,9 +26,9 @@ var aboutScene = new ScrollMagic.Scene({
     }
   });
 
-// Animación para after about section
+// Animación para redes section
 var contactScene = new ScrollMagic.Scene({
-  triggerElement: '.section#aftabout',
+  triggerElement: '.section#redes',
   triggerHook: 0.8,
   duration: '80%',
 })
@@ -36,42 +36,16 @@ var contactScene = new ScrollMagic.Scene({
   .on('progress', function (e) {
     var progress = e.progress.toFixed(2);
     // Show content
-    var contentElement = document.querySelector('.section#aftabout .container');
+    var contentElement = document.querySelector('.section#redes .container');
     if (contentElement) {
-      var contentOpacity = progress < 0.1 ? 0 : (progress - 0.5) * 5;
-      contentElement.style.opacity = contentOpacity;
-    }
-  });
-
-
-// Animación para contact section
-var contactScene = new ScrollMagic.Scene({
-  triggerElement: '.section#befcontact',
-  triggerHook: 0.8,
-  duration: '80%',
-})
-  .addTo(controller)
-  .on('progress', function (e) {
-    var progress = e.progress.toFixed(2);
-    var scrollPos = progress * 400;
-    var gradientColors = 'linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(232, 70, 59, 1), rgba(237, 187, 28, 1), rgba(255, 255, 255, 1), rgba(0, 0, 0, 1) ' + scrollPos + '%)';
-    document.querySelector('.section#befcontact').style.background = gradientColors;
-
-    // Parallax Effect 
-    var parallaxOffset = progress * 20;
-    document.querySelector('.section#befcontact').style.backgroundPositionY = '-' + parallaxOffset + 'px';
-
-    // Show content
-    var contentElement = document.querySelector('.section#befcontact .container');
-    if (contentElement) {
-      var contentOpacity = progress < 0.1 ? 1 : (progress - 0.8) * 2;
+      var contentOpacity = progress < 0.1 ? 0 : (progress - 0.1) * 5;
       contentElement.style.opacity = contentOpacity;
     }
   });
 
   // Animación para eventos section
 var contactScene = new ScrollMagic.Scene({
-  triggerElement: '.section#events',
+  triggerElement: '.section#redes',
   triggerHook: 0.8,
   duration: '80%',
 })
@@ -86,15 +60,39 @@ var contactScene = new ScrollMagic.Scene({
     }
   });
 
+  // Animación para beforecontact section
+var contactScene = new ScrollMagic.Scene({
+  triggerElement: '.section#befcontact',
+  triggerHook: 0.8,
+  duration: '80%',
+})
+  .addTo(controller)
+  .on('progress', function (e) {
+    var progress = e.progress.toFixed(2);
+    var scrollPos = progress * 400;
+    var gradientColors = 'linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(232, 70, 59, 1), rgba(237, 187, 28, 1), rgba(0, 0, 0, 1) ' + scrollPos + '%)';
+    document.querySelector('.section#befcontact').style.background = gradientColors;
 
-// Smooth scrolling
+    // Parallax Effect 
+    var parallaxOffset = progress * 20;
+    document.querySelector('.section#befcontact').style.backgroundPositionY = '-' + parallaxOffset + 'px';
+
+    // Show content
+    var contentElement = document.querySelector('.section#befcontact .container');
+    if (contentElement) {
+      var contentOpacity = progress < 0.1 ? 1 : (progress - 0.8) * 5;
+      contentElement.style.opacity = contentOpacity;
+    }
+  });
+
+
+
+// NavSmooth scrolling
 document.querySelectorAll('nav .nav-links li a').forEach(function (link) {
   link.addEventListener('click', function (e) {
     e.preventDefault();
-
     var target = document.querySelector(this.getAttribute('href'));
     var offset = target.offsetTop;
-
     window.scrollTo({
       top: offset,
       behavior: 'smooth'
